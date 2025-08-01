1f418efd7f542f185356c7403a4784d74f96ed7c
HyunKang Heo <149032548+Heoboss@users.noreply.github.com>
2025-08-01 16:48:06 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++11/백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <stack>
+#include <vector>
+#include <cstring>
+using namespace std;
+typedef long long ll;
+ll dp[91];
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+
+	cin >> n;
+	dp[1] = 1;
+	dp[2] = 1;
+	for (int i = 3; i <= n; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	cout << dp[n];
+	return 0;
+}
