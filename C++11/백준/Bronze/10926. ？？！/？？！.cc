30aafb94d6f238617a595562d1c853c06ef723c6
HyunKang Heo <149032548+Heoboss@users.noreply.github.com>
2025-08-03 11:56:55 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++11/백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_set>
+#include <unordered_map>
+#include <set>
+#include <map>
+using namespace std;
+
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	string s;
+	cin >> s;
+	cout << s + "??!";
+
+	return 0;
+}
