8e0fa8316c308879fb0763bbbcb04e4e3a695920
HyunKang Heo <149032548+Heoboss@users.noreply.github.com>
2025-08-04 13:34:16 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++11/백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <stack>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	string s;
+	cin >> s;
+	cout << s.length();
+	return 0;
+}
