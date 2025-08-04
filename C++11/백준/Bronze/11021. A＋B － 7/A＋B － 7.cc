9b1f929c4117b855a79b63e0c9b4dddd58a27c76
HyunKang Heo <149032548+Heoboss@users.noreply.github.com>
2025-08-04 10:04:47 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++11/백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <tuple>
+#include <stack>
+#include <vector>
+#include <cstring>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int t;
+	cin >> t;
+	int tt = 1;
+	while (t--) {
+		int a, b;
+		cin >> a >> b;
+		cout << "Case #"<<tt << ": " << a + b << '\n';
+		tt++;
+	}
+	return 0;
+}
