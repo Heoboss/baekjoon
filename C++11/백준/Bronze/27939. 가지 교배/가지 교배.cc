bf983234e92d95ab1c33cb15db7485a87708fed0
HyunKang Heo <149032548+Heoboss@users.noreply.github.com>
2025-08-19 09:09:10 +0900

    [Bronze I] Title: 가지 교배, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++11/백준/Bronze/27939. 가지 교배/가지 교배.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n, w = 0;
+  cin >> n;
+  vector<char> v(n);
+  for (int i = 0; i < n; i++)
+    cin >> v[i];
+  int m, k;
+  cin >> m >> k;
+  for (int i = 0; i < m; i++) {
+    int p = 0;
+    for (int j = 0; j < k; j++) {
+      int x;
+      cin >> x;
+      if (v[x - 1] == 'P')
+        p++;
+    }
+    if (p == 0)
+      w++;
+  }
+  if (w != 0)
+    cout << 'W';
+  else
+    cout << 'P';
+  return 0;
+}
